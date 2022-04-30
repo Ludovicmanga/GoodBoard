d57867869857d14e4189fac63b204e2b19726cc5
Ludovic <ludovic.mangaj@gmail.com>
2022-04-30 20:14:18 +0200

    the entity were created

---
+++ models/featureRequest.model.tsx
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+export {};
+
+const featureRequestSchema = new mongoose.Schema(
+{
+    title :{
+        type: String,
+        max: 1024
+    },
+    details :{
+        type: String,
+        max: 1024
+    },
+    votes: {
+        type: Number
+    },
+    creatorType: {
+        type: String,
+        max: 1024
+    },
+    status: {
+        type: String,
+        max: 1024
+    }
+},
+  {
+    timestamps: true,
+  }
+
+)
+
+const featureRequestModel = mongoose.model("Feature Request", featureRequestSchema);
+
+module.exports = featureRequestModel;
