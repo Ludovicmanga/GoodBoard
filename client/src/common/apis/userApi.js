3a1a6cbb76ba9d9610038003ee1f6c87ea68f580
Ludovic <ludovic.mangaj@gmail.com>
2022-05-06 16:11:08 +0200

    changes

--- client/src/common/apis/userApi.js
+++ client/src/common/apis/userApi.js
@@ -1,1 +1,9 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+exports.__esModule = true;
+var axios_1 = __importDefault(require("axios"));
+exports["default"] = axios_1["default"].create({
+    baseURL: "http://localhost:5000"
+});
