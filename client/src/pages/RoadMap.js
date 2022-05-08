b63537a8edd4860345a74d5ac80d0eb9306fcdb4
Ludovic <ludovic.mangaj@gmail.com>
2022-05-08 23:47:14 +0200

    the roadmap page was created

--- client/src/pages/RoadMap.js
+++ client/src/pages/RoadMap.js
@@ -5,9 +5,33 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 exports.__esModule = true;
 exports.RoadMap = void 0;
 var react_1 = __importDefault(require("react"));
+var react_redux_1 = require("react-redux");
+var FeatureRequestsBox_1 = require("../components/FeatureRequest/FeatureRequestsBox/FeatureRequestsBox");
 var RoadMap = function (_a) {
     var handleCurrentPage = _a.handleCurrentPage;
     handleCurrentPage('Roadmap');
-    return (react_1["default"].createElement("div", null, "Roadmap"));
+    var allFeatureRequests = (0, react_redux_1.useSelector)(function (state) { return state.allFeatureRequestsReducer; });
+    return (react_1["default"].createElement("div", { className: 'roadmap--container' },
+        react_1["default"].createElement("div", { className: 'roadmap--planned-column roadmap-column' },
+            react_1["default"].createElement("h2", { className: 'roadmap-column--title' }, "Planned"),
+            allFeatureRequests.map(function (featureRequest) {
+                if (featureRequest.status === "planned") {
+                    return (react_1["default"].createElement(FeatureRequestsBox_1.FeatureRequestsBox, { key: featureRequest._id, title: featureRequest.title, details: featureRequest.details, votes: featureRequest.voters.length, featureRequestId: featureRequest._id }));
+                }
+            })),
+        react_1["default"].createElement("div", { className: 'roadmap--in-progress-column roadmap-column' },
+            react_1["default"].createElement("h2", { className: 'roadmap-column--title' }, "In progress"),
+            allFeatureRequests.map(function (featureRequest) {
+                if (featureRequest.status === "in-progress") {
+                    return (react_1["default"].createElement(FeatureRequestsBox_1.FeatureRequestsBox, { key: featureRequest._id, title: featureRequest.title, details: featureRequest.details, votes: featureRequest.voters.length, featureRequestId: featureRequest._id }));
+                }
+            })),
+        react_1["default"].createElement("div", { className: 'roadmap--done-column roadmap-column' },
+            react_1["default"].createElement("h2", { className: 'roadmap-column--title' }, "Done"),
+            allFeatureRequests.map(function (featureRequest) {
+                if (featureRequest.status === "done") {
+                    return (react_1["default"].createElement(FeatureRequestsBox_1.FeatureRequestsBox, { key: featureRequest._id, title: featureRequest.title, details: featureRequest.details, votes: featureRequest.voters.length, featureRequestId: featureRequest._id }));
+                }
+            }))));
 };
 exports.RoadMap = RoadMap;
--- client/src/pages/RoadMap.tsx
+++ client/src/pages/RoadMap.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { FeatureRequestsBox } from '../components/FeatureRequest/FeatureRequestsBox/FeatureRequestsBox'
 
 type RoadMapProps = {
     handleCurrentPage: (page: string) => void
@@ -6,8 +8,64 @@ type RoadMapProps = {
 
 export const RoadMap: React.FC<RoadMapProps> = ({ handleCurrentPage }) => {
         handleCurrentPage('Roadmap')
+        const allFeatureRequests = useSelector((state: any) => state.allFeatureRequestsReducer);
         
         return (
-            <div>Roadmap</div>
+            <div className='roadmap--container'>
+                <div className='roadmap--planned-column roadmap-column'>
+                    <h2 className='roadmap-column--title'>Planned</h2>
+                    {
+                        allFeatureRequests.map((featureRequest) => {
+                            if (featureRequest.status === "planned") {
+                                return (
+                                    <FeatureRequestsBox
+                                        key = {featureRequest._id}
+                                        title = {featureRequest.title}
+                                        details = {featureRequest.details}
+                                        votes = {featureRequest.voters.length}
+                                        featureRequestId = {featureRequest._id}
+                                    />
+                                )
+                            } 
+                        })
+                    }
+                </div>
+                <div className='roadmap--in-progress-column roadmap-column'>
+                    <h2 className='roadmap-column--title'>In progress</h2>
+                    {
+                        allFeatureRequests.map((featureRequest) => {
+                            if (featureRequest.status === "in-progress") {
+                                return (
+                                    <FeatureRequestsBox
+                                        key = {featureRequest._id}
+                                        title = {featureRequest.title}
+                                        details = {featureRequest.details}
+                                        votes = {featureRequest.voters.length}
+                                        featureRequestId = {featureRequest._id}
+                                    />
+                                )
+                            } 
+                        })
+                    }
+                </div>
+                <div className='roadmap--done-column roadmap-column'>
+                    <h2 className='roadmap-column--title'>Done</h2>
+                    {
+                        allFeatureRequests.map((featureRequest) => {
+                            if (featureRequest.status === "done") {
+                                return (
+                                    <FeatureRequestsBox
+                                        key = {featureRequest._id}
+                                        title = {featureRequest.title}
+                                        details = {featureRequest.details}
+                                        votes = {featureRequest.voters.length}
+                                        featureRequestId = {featureRequest._id}
+                                    />
+                                )
+                            } 
+                        })
+                    }
+                </div>
+            </div>
         );
 }
