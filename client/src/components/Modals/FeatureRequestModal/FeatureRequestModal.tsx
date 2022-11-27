066fc17077babd38ec302ff1267ec5b591c58930
Ludovicmanga <ludovic.mangaj@gmail.com>
2022-11-27 19:21:39 +0100

    the upsert feature is working

--- client/src/components/Modals/FeatureRequestModal/FeatureRequestModal.tsx
+++ client/src/components/Modals/FeatureRequestModal/FeatureRequestModal.tsx
@@ -1,16 +1,29 @@
-import * as React from 'react';
-import Backdrop from '@mui/material/Backdrop';
-import Modal from '@mui/material/Modal';
-import Fade from '@mui/material/Fade';
-import Button from '@mui/material/Button';
-import { Avatar, AvatarGroup, Divider, MenuItem, Select, SelectChangeEvent, TextField } from '@mui/material';
-import styles from './FeatureRequestModal.module.scss';
-import ludoPhoto from '../../../photos/ludoImg.jpg';
-import noVoter from '../../../photos/no_voter.png';
-import { useState } from 'react';
-import { FeatureRequest, FeatureRequestModalMode, FeatureRequestStatus } from '../../../helpers/types';
-import { useEffect } from 'react';
-import { capitalizeFirstLetter } from '../../../helpers/utils';
+import * as React from "react";
+import Backdrop from "@mui/material/Backdrop";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+import Button from "@mui/material/Button";
+import {
+  Avatar,
+  AvatarGroup,
+  Divider,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  TextField,
+} from "@mui/material";
+import styles from "./FeatureRequestModal.module.scss";
+import ludoPhoto from "../../../photos/ludoImg.jpg";
+import noVoter from "../../../photos/no_voter.png";
+import { useState } from "react";
+import {
+  FeatureRequest,
+  FeatureRequestModalMode,
+  FeatureRequestStatus,
+} from "../../../helpers/types";
+import { useEffect } from "react";
+import { capitalizeFirstLetter } from "../../../helpers/utils";
+import axios from "axios";
 
 export default function FeatureRequestModal(props: {
   modalMode: FeatureRequestModalMode;
@@ -18,105 +31,137 @@ export default function FeatureRequestModal(props: {
   handleCloseModal: () => void;
   featureRequestProperties?: FeatureRequest;
 }) {
-  const [featureRequestProperties, setFeatureRequestProperties] = useState<FeatureRequest>({
-    _id: '',
-    title: '',
-    details: '',
-    voters: [],
-    creatorType: 'admin',
-    status: FeatureRequestStatus.unassigned,
-    creator: '',
-    createdAt: '',
-    updatedAt: '',
-});
+  const [featureRequestProperties, setFeatureRequestProperties] =
+    useState<FeatureRequest>({
+      _id: "",
+      title: "",
+      details: "",
+      voters: [],
+      creatorType: "admin",
+      status: FeatureRequestStatus.unassigned,
+      creator: "",
+      createdAt: "",
+      updatedAt: "",
+    });
 
   useEffect(() => {
-    if (props.featureRequestProperties && props.modalMode === FeatureRequestModalMode.update && props.modalIsOpen) {
-      setFeatureRequestProperties(props.featureRequestProperties)
+    if (
+      props.featureRequestProperties &&
+      props.modalMode === FeatureRequestModalMode.update &&
+      props.modalIsOpen
+    ) {
+      setFeatureRequestProperties(props.featureRequestProperties);
     }
-  }, [props.modalIsOpen, props.featureRequestProperties, props.modalMode])
+  }, [props.modalIsOpen, props.featureRequestProperties, props.modalMode]);
+
+  const handleUpsertRequest = async () => {
+    const upsertedFeatureRequest = await axios({
+      url: 'http://localhost:8080/feature-request/upsert',
+      method: 'post',
+      data: {
+          featureRequest: featureRequestProperties,
+      }
+    });
+    console.log(upsertedFeatureRequest, ' is the upserted feature request, i sent ', featureRequestProperties)
+  };
 
   return (
-      <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        open={props.modalIsOpen}
-        onClose={props.handleCloseModal}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
-        }}
-      >
-        <Fade in={props.modalIsOpen}>
-          <div className={styles.modalContentContainer}>
-            <div className={styles.modalTitle}>
-              { props.modalMode === FeatureRequestModalMode.creation ? 'Make' : 'Update' } a feature request
-            </div>
-            <Divider className={styles.divider} />
-            { props.modalMode === FeatureRequestModalMode.update && (
-              <>
-                <div className={styles.votersSection}>
-                <div className={styles.votersSectionTitle}>
-                  Voters :
-                </div>
-                <AvatarGroup className={styles.avatarGroup} total={featureRequestProperties.voters?.length}>
-                  { featureRequestProperties.voters.length > 1 ? featureRequestProperties.voters?.slice(0,4).map( voter =>
-                      <Avatar key={voter} alt="Voters pic" src={ludoPhoto} />
-                  ) : <div>Not voted yet</div>}
-                </AvatarGroup>
-                </div>
-                <div className={styles.statusSection}>
-                  <div className={styles.statusSectionTitle}>
-                    Status :
-                  </div>
-                  <Select
-                    labelId="status"
-                    id="status"
-                    value={featureRequestProperties.status}
-                    label="Status"
-                    onChange={(e: SelectChangeEvent<string>) => {
-                      setFeatureRequestProperties( propertiesState => {
-                        return { ...propertiesState, status: e.target.value }
-                      } )
-                    }}
-                    >
-                    {
-                      (Object.keys(FeatureRequestStatus) as Array<keyof typeof FeatureRequestStatus>).map((status) =>
-                        <MenuItem value={status}>{capitalizeFirstLetter(status)}</MenuItem>
-                      )
-                    }
-                  </Select>
-                </div>
-              </>
-            )}
-            <TextField
-              label="Titre"
-              variant="filled"
-              value={featureRequestProperties.title}
-              onChange={(e) => {
-                setFeatureRequestProperties( propertiesState => {
-                  return { ...propertiesState, title: e.target.value }
-                } )
-              }}
-              className={styles.textInput}
-            />
-            <TextField
-              label="Description"
-              variant="filled"
-              value={featureRequestProperties.details}
-              onChange={(e) => {
-                setFeatureRequestProperties( propertiesState => {
-                  return { ...propertiesState, details: e.target.value }
-                } )
-              }}
-              className={styles.textInput}
-            />
-            <div className={styles.submitButtonContainer}>
-              <Button variant="contained">Add request</Button>
-            </div>
+    <Modal
+      aria-labelledby="transition-modal-title"
+      aria-describedby="transition-modal-description"
+      open={props.modalIsOpen}
+      onClose={props.handleCloseModal}
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        timeout: 500,
+      }}
+    >
+      <Fade in={props.modalIsOpen}>
+        <div className={styles.modalContentContainer}>
+          <div className={styles.modalTitle}>
+            {props.modalMode === FeatureRequestModalMode.creation
+              ? "Make"
+              : "Update"}{" "}
+            a feature request
           </div>
-        </Fade>
-      </Modal>
+          <Divider className={styles.divider} />
+          {props.modalMode === FeatureRequestModalMode.update && (
+            <>
+              <div className={styles.votersSection}>
+                <div className={styles.votersSectionTitle}>Voters :</div>
+                <AvatarGroup
+                  className={styles.avatarGroup}
+                  total={featureRequestProperties.voters?.length}
+                >
+                  {featureRequestProperties.voters.length > 1 ? (
+                    featureRequestProperties.voters
+                      ?.slice(0, 4)
+                      .map((voter) => (
+                        <Avatar key={voter} alt="Voters pic" src={ludoPhoto} />
+                      ))
+                  ) : (
+                    <div>Not voted yet</div>
+                  )}
+                </AvatarGroup>
+              </div>
+              <div className={styles.statusSection}>
+                <div className={styles.statusSectionTitle}>Status :</div>
+                <Select
+                  labelId="status"
+                  id="status"
+                  value={featureRequestProperties.status}
+                  label="Status"
+                  onChange={(e: SelectChangeEvent<string>) => {
+                    setFeatureRequestProperties((propertiesState) => {
+                      return { ...propertiesState, status: e.target.value };
+                    });
+                  }}
+                >
+                  {(
+                    Object.keys(FeatureRequestStatus) as Array<
+                      keyof typeof FeatureRequestStatus
+                    >
+                  ).map((status) => (
+                    <MenuItem value={status}>
+                      {capitalizeFirstLetter(status)}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </div>
+            </>
+          )}
+          <TextField
+            label="Titre"
+            variant="filled"
+            value={featureRequestProperties.title}
+            onChange={(e) => {
+              setFeatureRequestProperties((propertiesState) => {
+                return { ...propertiesState, title: e.target.value };
+              });
+            }}
+            className={styles.textInput}
+          />
+          <TextField
+            label="Description"
+            variant="filled"
+            value={featureRequestProperties.details}
+            onChange={(e) => {
+              setFeatureRequestProperties((propertiesState) => {
+                return { ...propertiesState, details: e.target.value };
+              });
+            }}
+            className={styles.textInput}
+          />
+          <Button
+            onClick={handleUpsertRequest}
+            className={styles.submitButton}
+            variant="contained"
+          >
+            Add request
+          </Button>
+        </div>
+      </Fade>
+    </Modal>
   );
 }
--- controllers/featureRequest.controller.ts
+++ controllers/featureRequest.controller.ts
@@ -1,61 +1,91 @@
-import featureRequestModel from '../models/featureRequest.model';
-import mongoose from 'mongoose';
-import userModel from '../models/user.model';
+import featureRequestModel from "../models/featureRequest.model";
+import mongoose from "mongoose";
+import userModel from "../models/user.model";
+import { request } from "express";
 
 const ObjectId = mongoose.Types.ObjectId;
 
 export const getAllFeatureRequests = async (req, res) => {
-    await featureRequestModel.find()
-        .then(allFeatureRequests => res.status(200).send(allFeatureRequests));
-}
+  await featureRequestModel
+    .find()
+    .then((allFeatureRequests) => res.status(200).send(allFeatureRequests));
+};
 
 export const getAllCompanyFeatureRequests = async (req, res) => {
-    await featureRequestModel.find({ creatorType: "company" })
-        .then(allCompanyFeatureRequests => res.status(200).send(allCompanyFeatureRequests));
-}
+  await featureRequestModel
+    .find({ creatorType: "company" })
+    .then((allCompanyFeatureRequests) =>
+      res.status(200).send(allCompanyFeatureRequests)
+    );
+};
 export const getAllUserFeatureRequests = async (req, res) => {
-    await featureRequestModel.find({ creatorType: "user" })
-        .then(allUserFeatureRequests => res.status(200).send(allUserFeatureRequests));
-}
+  await featureRequestModel
+    .find({ creatorType: "user" })
+    .then((allUserFeatureRequests) =>
+      res.status(200).send(allUserFeatureRequests)
+    );
+};
+
+export const upsertFeatureRequest = async (req, res) => {
+  featureRequestModel.findOneAndUpdate(
+    { _id: req.body.featureRequest._id },
+    {
+      title: req.body.featureRequest.title,
+      details: req.body.featureRequest.details,
+      creatorType: req.body.featureRequest.creatorType,
+      status: req.body.featureRequest.status,
+      creator: req.body.featureRequest.creator,
+    },
+    {
+      new: true,
+      upsert: true,
+    },
+    function (err, upsertedFeatureRequest) {
+      if (err) return res.status(500).send({ error: err });
+      res.status(200).send(upsertedFeatureRequest);
+    }
+  );
+};
 
 export const createFeatureRequest = (req, res) => {
-    const newFeatureRequest = new featureRequestModel({
-        title: req.body.title,
-        details: req.body.details,
-        creatorType: req.body.creatorType,
-        status: req.body.status,
-        creator: req.body.creator
-    });
+  const newFeatureRequest = new featureRequestModel({
+    title: req.body.title,
+    details: req.body.details,
+    creatorType: req.body.creatorType,
+    status: req.body.status,
+    creator: req.body.creator,
+  });
 
-    newFeatureRequest.save()
-        .then(featureRequest => res.status(200).send(featureRequest))
-        .catch(error => console.log(error))
-}
+  newFeatureRequest
+    .save()
+    .then((featureRequest) => res.status(200).send(featureRequest))
+    .catch((error) => console.log(error));
+};
 
 export const upVote = async (req, res) => {
-    const updatedFeatureRequest = await featureRequestModel.updateOne(
-        { _id: req.params.id },
-        { $addToSet: { voters: req.body.userId } }
-    )
+  const updatedFeatureRequest = await featureRequestModel.updateOne(
+    { _id: req.params.id },
+    { $addToSet: { voters: req.body.userId } }
+  );
 
-    await userModel.updateOne(
-        { _id: req.body.userId },
-        { $addToSet: { voted: req.params.id } }
-    )
-    
-    res.status(200).json({ updatedFeatureRequest });
-}
+  await userModel.updateOne(
+    { _id: req.body.userId },
+    { $addToSet: { voted: req.params.id } }
+  );
+
+  res.status(200).json({ updatedFeatureRequest });
+};
 
 export const downVote = async (req, res) => {
-    const updatedFeatureRequest = await featureRequestModel.updateOne(
-        { _id: req.params.id },
-        { $pull: { voters: req.body.userId } }
-    )
+  const updatedFeatureRequest = await featureRequestModel.updateOne(
+    { _id: req.params.id },
+    { $pull: { voters: req.body.userId } }
+  );
 
-    await userModel.updateOne(
-        { _id: req.body.userId },
-        { $pull: { voted: req.params.id } }
-    )
+  await userModel.updateOne(
+    { _id: req.body.userId },
+    { $pull: { voted: req.params.id } }
+  );
 
-    res.status(200).json({ updatedFeatureRequest });
-}
+  res.status(200).json({ updatedFeatureRequest });
+};
