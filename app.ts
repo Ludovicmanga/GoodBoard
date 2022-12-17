cd0c15cfb760b4fce92123f98bd4d7bf4b80f507
Ludovicmanga <ludovic.mangaj@gmail.com>
2022-12-17 22:58:34 +0100

    deployment test

--- app.ts
+++ app.ts
@@ -5,6 +5,7 @@ import './config/db.ts';
 import cors from 'cors';
 import session from 'express-session';
 import MongoStore from 'connect-mongo';
+import path from 'path';
 
 const corsOptions = {
     origin: 'http://localhost:3000',
@@ -29,7 +30,11 @@ var app = express();
 
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use(express.static('client/build'));
+app.use(express.static(path.join(__dirname, "client/build")));
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
 app.use(express.urlencoded({ extended: false }));
 app.use(session({
   secret: 'this is my secrethkjrhkfrhkfh',
@@ -64,12 +69,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.json({ error: err })
 });
-
-app.get('/test', (_, res) => {
-  res.send({
-    message: 'working',
-  })
-})
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`le serveur est lancé sur le port ${PORT}`);
