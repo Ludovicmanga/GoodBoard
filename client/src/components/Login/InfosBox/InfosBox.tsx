442645b353b14abf5d22971f6429910443c398a1
Ludovic Manga <ludovicmanga@Ludovics-MacBook-Air.local>
2024-04-28 23:08:01 +0200

    changes front

---
+++ client/src/components/Login/InfosBox/InfosBox.tsx
@@ -0,0 +1,10 @@
+import { GoodboardLogo } from "../../Logo/GoodboardLogo";
+import styles from "./InfosBox.module.scss";
+
+export const InfosBox = () => {
+  return (
+    <div className={styles.container}>
+      <GoodboardLogo />
+    </div>
+  );
+};
