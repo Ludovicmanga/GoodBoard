bbb0a62123980cb0bd935a1c18f3644abcb80b59
Ludovic Manga <ludovicmanga@Ludovics-MacBook-Air.local>
2024-05-05 19:50:25 +0200

    the filter bar was improved a lot

--- client/src/pages/FeatureRequests/FeatureRequests.tsx
+++ client/src/pages/FeatureRequests/FeatureRequests.tsx
@@ -17,6 +17,8 @@ import MainHero from "../../components/MainHero/MainHero";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import FilterFeatureRequestsSidebar from "../../components/FilterFeatureRequestsSidebar/FilterFeatureRequestsSidebar";
 import LoadingSkeleton from "../../components/LoadingSkeleton/LoadingSkeleton";
+import { Chip, IconButton, ListItem, Popover } from "@mui/material";
+import { Filter1, FilterList, Search, SwapVert } from "@mui/icons-material";
 
 type Props = {};
 
@@ -24,7 +26,7 @@ const FeatureRequests = (props: Props) => {
   const [selectedStatus, setSelectedStatus] = useState<string | null>(null);
   const [selectedTopic, setSelectedTopic] = useState<string | null>(null);
   const [searchedWord, setSearchedWord] = useState<string | null>(null);
-/*   const [filteredFeatureRequests, setFilteredFeatureRequests] = useState<
+  /*   const [filteredFeatureRequests, setFilteredFeatureRequests] = useState<
     FeatureRequest[]
   >([]); */
   const activeBoardState = useAppSelector((state) => state.activeBoard);
@@ -35,13 +37,13 @@ const FeatureRequests = (props: Props) => {
   const allFeatureRequests = useAppSelector(
     (state) => state.allFeatureRequests
   );
-/* 
+  /* 
   const dispatch = useAppDispatch();
   const menuSelectedState = useAppSelector(
     (state) => state.generalProperties.menuSelected
   ); */
 
-/*   const handleSetCorrespondingFeatures = () => {
+  /*   const handleSetCorrespondingFeatures = () => {
     if (props.type === UserType.externalUser) {
       const featureRequestsWithCorrespondingPropsType =
         allFeatureRequests.filter(
@@ -74,7 +76,7 @@ const FeatureRequests = (props: Props) => {
       );
     }
   }, [menuSelectedState]); */
-/* 
+  /* 
   useEffect(() => {
     handleSetCorrespondingFeatures();
   }, [allFeatureRequests, props.type]); */
@@ -90,6 +92,20 @@ const FeatureRequests = (props: Props) => {
     }
   };
 
+  const [chipData, setChipData] = React.useState<
+    { key: number; label: string }[]
+  >([
+    { key: 0, label: "Angular" },
+    { key: 1, label: "jQuery" },
+    { key: 2, label: "Polymer" },
+    { key: 3, label: "React" },
+    { key: 4, label: "Vue.js" },
+  ]);
+
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
+    null
+  );
+
   return (
     <>
       <MainNavBar />
@@ -97,76 +113,83 @@ const FeatureRequests = (props: Props) => {
       {generalPropertiesState.featuresAreLoading ? (
         <LoadingSkeleton />
       ) : (
-        <div className={styles.sectionContainer}>
-          {activeBoardState.billingPlan !== BillingPlan.free && (
-            <div className={styles.sidebarContainer}>
-              <FilterFeatureRequestsSidebar
-                setSelectedTopic={setSelectedTopic}
-                handleChangeSelectedStatus={handleChangeSelectedStatus}
-                selectedTopic={selectedTopic}
-                selectedStatus={selectedStatus}
+        <div className={styles.container}>
+          {allFeatureRequests.length > 0 ? (
+            <div className={styles.featuresContainer}>
+              <div className={styles.filterSectionContainer}>
+                <IconButton
+                  size="small"
+                  className={styles.filterIconBtn}
+                  onClick={(e) => setAnchorEl(e.currentTarget)}
+                >
+                  <SwapVert />
+                  <div className={styles.filterIconText}>Filtrer</div>
+                </IconButton>
+                <IconButton size="small" className={styles.filterIconBtn}>
+                  <FilterList />
+                  <div className={styles.filterIconText}>Trier</div>
+                </IconButton>
+                <IconButton size="small" className={styles.filterIconBtn}>
+                  <Search />
+                  <div className={styles.filterIconText}>Recherche</div>
+                </IconButton>
+              </div>
+              <Popover
+                open={!!anchorEl}
+                anchorEl={anchorEl}
+                onClose={() => setAnchorEl(null)}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+              >
+                Here we arefrefekzlm
+              </Popover>
+              {allFeatureRequests
+                .filter((featReq) => {
+                  if (selectedTopic) {
+                    return featReq.topics.includes(selectedTopic!);
+                  } else {
+                    return featReq;
+                  }
+                })
+                .filter((featReq) => {
+                  if (selectedStatus) {
+                    return (
+                      featReq.status.toLowerCase() ===
+                      selectedStatus.toLowerCase()
+                    );
+                  } else {
+                    return featReq;
+                  }
+                })
+                .filter((featReq) => {
+                  if (searchedWord) {
+                    return featReq.title
+                      .toLowerCase()
+                      .includes(searchedWord.toLowerCase());
+                  } else {
+                    return featReq;
+                  }
+                })
+                .map((featureRequest) => {
+                  return (
+                    <FeatureRequestBox
+                      key={featureRequest._id}
+                      featureRequestProperties={featureRequest}
+                    />
+                  );
+                })}
+            </div>
+          ) : (
+            <div className={styles.emptyDataContainer}>
+              <EmptyData
+                title="No feature request yet !"
+                details="Setup your board with creative ideas"
+                type={EmptyPageType.featureRequests}
               />
             </div>
           )}
-          <div
-            className={
-              allFeatureRequests.length > 0
-                ? styles.featuresSectionContainer
-                : `${styles.featuresSectionContainer} ${styles.featuresSectionContainerEmpty}`
-            }
-          >
-            {allFeatureRequests.length > 0 ? (
-              <div className={styles.featuresContainer}>
-                <SearchBar
-                  onSearch={(searchedWord) => setSearchedWord(searchedWord)}
-                />
-
-                {allFeatureRequests
-                  .filter((featReq) => {
-                    if (selectedTopic) {
-                      return featReq.topics.includes(selectedTopic!);
-                    } else {
-                      return featReq;
-                    }
-                  })
-                  .filter((featReq) => {
-                    if (selectedStatus) {
-                      return (
-                        featReq.status.toLowerCase() ===
-                        selectedStatus.toLowerCase()
-                      );
-                    } else {
-                      return featReq;
-                    }
-                  })
-                  .filter((featReq) => {
-                    if (searchedWord) {
-                      return featReq.title
-                        .toLowerCase()
-                        .includes(searchedWord.toLowerCase());
-                    } else {
-                      return featReq;
-                    }
-                  })
-                  .map((featureRequest) => {
-                    return (
-                      <FeatureRequestBox
-                        key={featureRequest._id}
-                        featureRequestProperties={featureRequest}
-                      />
-                    );
-                  })}
-              </div>
-            ) : (
-              <div className={styles.emptyDataContainer}>
-                <EmptyData
-                  title="No feature request yet !"
-                  details="Setup your board with creative ideas"
-                  type={EmptyPageType.featureRequests}
-                />
-              </div>
-            )}
-          </div>
         </div>
       )}
       {allFeatureRequests.length > 0 && (
