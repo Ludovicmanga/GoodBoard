6eddaf7ae5d8eed5654696e4df1f24a78059cd20
Ludovic Manga <ludovicmanga@Ludovics-MacBook-Air.local>
2024-05-06 15:59:46 +0200

    darkmode btn fixed

--- client/src/components/buttons/DarkModeToggle/DarkModeToggle.tsx
+++ client/src/components/buttons/DarkModeToggle/DarkModeToggle.tsx
@@ -1,25 +1,32 @@
-import React, { useState } from 'react';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
-import { IconButton } from '@mui/material';
-import { useAppDispatch } from '../../../redux/hooks';
-import { setGeneralProperties } from '../../../redux/features/generalPropertiesSlice';
+import React, { useEffect, useState } from "react";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import { IconButton } from "@mui/material";
+import { useAppDispatch } from "../../../redux/hooks";
+import { setGeneralProperties } from "../../../redux/features/generalPropertiesSlice";
 
 function DarkModeToggle() {
-  const [isDarkMode, setIsDarkMode] = useState(localStorage.getItem('darkMode') === 'on');
+  const [isDarkMode, setIsDarkMode] = useState(
+    localStorage.getItem("darkMode") === "on"
+  );
   const dispatch = useAppDispatch();
 
   const handleToggle = () => {
-    setIsDarkMode(!isDarkMode);
-    dispatch(setGeneralProperties({
-      darkMode: isDarkMode ? true : false,
-    }))
-    localStorage.setItem('darkMode', !isDarkMode ? 'on' : 'off');
+    dispatch(
+      setGeneralProperties({
+        darkMode: Boolean(isDarkMode),
+      })
+    );
+    localStorage.setItem("darkMode", isDarkMode ? "on" : "off");
   };
 
+  useEffect(() => {
+    handleToggle();
+  }, [isDarkMode]);
+
   return (
-    <IconButton onClick={handleToggle}>
-      {isDarkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+    <IconButton onClick={() => setIsDarkMode(!isDarkMode)}>
+      {isDarkMode ? <Brightness4Icon /> : <Brightness7Icon />}
     </IconButton>
   );
 }
